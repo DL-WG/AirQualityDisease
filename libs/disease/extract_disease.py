2365a1f047032c5b8b24db96bb8d148ba9d4cbf6
FancyAI21 <h.fan21@imperial.ac.uk>
2023-02-02 23:23:08 +0800

    add plot

---
+++ libs/disease/extract_disease.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+
+def extract_disease(patient_path, diseases=['Cystic Fibrosis', 'COVID', 'ABPA', 'Asthma']):
+    df = pd.read_csv(patient_path)
+    dis_df = pd.DataFrame(columns=['postcode'] + diseases)
+    # dis_stat = pd.DataFrame(columns = ['postcode']+diseases)
+    dis_stat = {}
+    for idx, row in df.iterrows():
+        postcodes = eval(row['postcode and visit no']).keys()
+        # flag = [1 if row[d] else 0 for d in diseases]
+        flag = []
+        for disease in diseases:
+            if isinstance(row[disease], str) and row[disease]=='FALSE':
+                row[disease] = False
+            if row[disease]:
+                flag.append(1)
+            else:
+                flag.append(0)
+
+        for postcode in postcodes:
+            if postcode not in dis_stat.keys():
+                dis_stat[postcode] = np.zeros(len(diseases))
+            dis_stat[postcode] += flag
+
+    for postcode in dis_stat:
+        row = {name:value for name, value in zip(['postcode'] + diseases, [postcode] + list(dis_stat[postcode]))}
+        dis_df = dis_df.append(row, ignore_index=True)
+
+    return dis_df
+
+
