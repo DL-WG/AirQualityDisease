81c701456e785a484711d2adfef733c414494f61
FancyAI21 <h.fan21@imperial.ac.uk>
2023-02-17 22:00:12 +0800

    drop no CT

--- aq_disease_corr.py
+++ aq_disease_corr.py
@@ -3,16 +3,20 @@ import pandas as pd
 from scipy import stats
 
 
-def calc_corr(aq_path, dise_path, aq='no2', diseases=['Cystic Fibrosis', 'COVID', 'ABPA', 'Asthma'], min_patients=3):
+def calc_corr(aq_path, dise_path, aq='no2', diseases=['Cystic Fibrosis', 'COVID', 'ABPA', 'Asthma'],
+              min_patients=0, norm=True):
     df_aq = pd.read_excel(aq_path)
     df_dise = pd.read_excel(dise_path)
     # df_all = df_aq.merge(df_dise, on='postcode', how='left')
     corr = {}
     for disease in diseases:
         df_dise_sel = df_dise[df_dise[disease] > min_patients]
-        df_temp = df_aq.merge(df_dise_sel[['postcode', disease]], on='postcode', how='left')
+        df_temp = df_aq.merge(df_dise_sel[['postcode', disease, 'patient_num']], on='postcode', how='left')
         df_temp.dropna(inplace=True)
-        r, p_value = stats.pearsonr(df_temp[aq].values, df_temp[disease].values)
+        if norm:
+            r, p_value = stats.pearsonr(df_temp[aq].values, df_temp[disease].values/df_temp.patient_num.values)
+        else:
+            r, p_value = stats.pearsonr(df_temp[aq].values, df_temp[disease].values)
         corr[disease] = r
     return corr
 
@@ -21,7 +25,7 @@ if __name__ == '__main__':
     aq = 'no2'
     aq_path = f'data/postcode_{aq}.xlsx'
     dise_path = 'data/postcode_diseases.xlsx'
-    corr = calc_corr(aq_path, dise_path, aq=aq)
+    corr = calc_corr(aq_path, dise_path, aq=aq, diseases=['COVID'])
     print(corr)
 
 
--- libs/disease/extract_disease.py
+++ libs/disease/extract_disease.py
@@ -1,12 +1,22 @@
+import os
+import glob
 import pandas as pd
 import numpy as np
 
 
-def extract_disease(patient_path, diseases=['Cystic Fibrosis', 'COVID', 'ABPA', 'Asthma']):
+def extract_disease(patient_path, diseases=['Cystic Fibrosis', 'COVID', 'ABPA', 'Asthma'], drop_no_CT=True):
     df = pd.read_csv(patient_path)
-    dis_df = pd.DataFrame(columns=['postcode'] + diseases)
+    dis_df = pd.DataFrame(columns=['postcode', 'patient_num'] + diseases)
+    if drop_no_CT:
+        path = 'data/patient/london_CT_comments'
+        all_files = os.listdir(path)
+        csv_files = list(filter(lambda f: f.endswith('.csv'), all_files))
+        patient_id = [int(csv_name.split('_')[0]) for csv_name in csv_files]
+        df = df[df.PATIENT_RK.isin(patient_id)]
+
     # dis_stat = pd.DataFrame(columns = ['postcode']+diseases)
     dis_stat = {}
+    patient_num = {}
     for idx, row in df.iterrows():
         postcodes = eval(row['postcode and visit no']).keys()
         # flag = [1 if row[d] else 0 for d in diseases]
@@ -22,10 +32,13 @@ def extract_disease(patient_path, diseases=['Cystic Fibrosis', 'COVID', 'ABPA', 
         for postcode in postcodes:
             if postcode not in dis_stat.keys():
                 dis_stat[postcode] = np.zeros(len(diseases))
+                patient_num[postcode] = 0
             dis_stat[postcode] += flag
+            patient_num[postcode] += 1
 
     for postcode in dis_stat:
-        row = {name:value for name, value in zip(['postcode'] + diseases, [postcode] + list(dis_stat[postcode]))}
+        row = {name:value for name, value in zip(['postcode', 'patient_num'] + diseases,
+                                                 [postcode, patient_num[postcode]] + list(dis_stat[postcode]))}
         dis_df = dis_df.append(row, ignore_index=True)
 
     return dis_df
